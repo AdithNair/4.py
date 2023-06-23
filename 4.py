fcd263f21925c5774b33768e89fe6bdbd659a03c
AdithNair <104628360+AdithNair@users.noreply.github.com>
2023-06-23 11:03:25 -0400

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+subject= ['Math','Science','ELA','Social Studies']
+print(subject)
+
+superhero= ['Hulk','Iron Man','Falcon','Thor']
+print(superhero)  
+
+sports= ['Soccer','Basketball',"Football",'Tennis']
+print(sports)
